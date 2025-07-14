344891312c667a40b1f2c10492f3d552ee7dc4ef
H-Shamkhani <shamkhanihossein93@gmail.com>
2025-07-14 23:38:08 +0330

    Implement pickers

---
+++ lib/src/main/java/com/hshamkhani/persiandtpicker/picker/PersianDatePicker.kt
@@ -0,0 +1,145 @@
+package com.hshamkhani.persiandtpicker.picker
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.hshamkhani.persiandtpicker.components.WheelPicker
+import com.hshamkhani.persiandtpicker.utils.DateUtils
+import com.hshamkhani.persiandtpicker.utils.PersianNumberUtils.formatToHindiIfLanguageIsFa
+import com.hshamkhani.persiandtpicker.utils.PersianNumberUtils.toArabicDigits
+import ir.huri.jcal.JalaliCalendar
+
+@Composable
+fun PersianDatePicker(
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.headlineMedium,
+    fontFamily: FontFamily = FontFamily.Default,
+    textColor: Color = MaterialTheme.colorScheme.onSurface,
+    selectedTextColor: Color = MaterialTheme.colorScheme.primary,
+    backGroundColor: Color = MaterialTheme.colorScheme.background,
+    selectedItemBackgroundColor: Color = MaterialTheme.colorScheme.surfaceContainerLowest,
+    selectedItemBackgroundClipSize: Dp = 16.dp,
+    isEnglish: Boolean? = null,
+    onDateSelected: (jYear: Int, jMonth: Int, jDay: Int) -> Unit,
+) {
+
+    val isEng = isEnglish ?: (Locale.current.language != "fa")
+
+    val initialDate by remember {
+        mutableStateOf(JalaliCalendar())
+    }
+
+    var selectedDate by remember {
+        mutableStateOf(initialDate)
+    }
+
+    val years by remember {
+        mutableStateOf(
+            DateUtils.initYearList(initialDate.year).map { it.toString().formatToHindiIfLanguageIsFa() })
+    }
+
+    val months by remember {
+        mutableStateOf(
+            DateUtils.initMonthList(isEng = isEng)
+        )
+    }
+
+    val days by remember(selectedDate.month) {
+        if (selectedDate.monthLength == 30 && selectedDate.day == 31) {
+            selectedDate.day -= 1
+        }
+        mutableStateOf(
+            DateUtils.initDaysList(selectedDate.monthLength).map { it }
+        )
+    }
+
+    LaunchedEffect(selectedDate) {
+        onDateSelected(
+            selectedDate.year,
+            selectedDate.month,
+            selectedDate.day,
+        )
+    }
+
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            WheelPicker(
+                modifier = Modifier,
+                options = days,
+                initialValue = initialDate.day - 1,
+                fontFamily = fontFamily,
+                textStyle = textStyle,
+                textColor = textColor,
+                selectedTextColor = selectedTextColor,
+                backGroundColor = backGroundColor,
+                selectedItemBackgroundColor = selectedItemBackgroundColor,
+                selectedItemBackgroundClipSize = selectedItemBackgroundClipSize,
+                onValueSelected = { index, _ ->
+                    selectedDate = JalaliCalendar().apply {
+                        day = index + 1
+                        month = selectedDate.month
+                        year = selectedDate.year
+                    }
+                }
+            )
+            WheelPicker(
+                options = months,
+                initialValue = initialDate.month - 1,
+                fontFamily = fontFamily,
+                textStyle = textStyle,
+                textColor = textColor,
+                selectedTextColor = selectedTextColor,
+                backGroundColor = backGroundColor,
+                selectedItemBackgroundColor = selectedItemBackgroundColor,
+                selectedItemBackgroundClipSize = selectedItemBackgroundClipSize,
+                onValueSelected = { index, _ ->
+                    selectedDate = JalaliCalendar().apply {
+                        day = selectedDate.day
+                        month = index + 1
+                        year = selectedDate.year
+                    }
+                }
+            )
+            WheelPicker(
+                options = years,
+                initialValue = 1,
+                fontFamily = fontFamily,
+                textStyle = textStyle,
+                textColor = textColor,
+                selectedTextColor = selectedTextColor,
+                backGroundColor = backGroundColor,
+                selectedItemBackgroundColor = selectedItemBackgroundColor,
+                selectedItemBackgroundClipSize = selectedItemBackgroundClipSize,
+                onValueSelected = { index, _ ->
+                    selectedDate = JalaliCalendar().apply {
+                        day = selectedDate.day
+                        month = selectedDate.month
+                        year = years[index].toArabicDigits()
+                    }
+                }
+            )
+        }
+    }
+}
--- sample/src/main/java/com/hshamkhani/persian_dtpicker/MainActivity.kt
+++ sample/src/main/java/com/hshamkhani/persian_dtpicker/MainActivity.kt
@@ -4,44 +4,78 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hshamkhani.persian_dtpicker.ui.theme.PersiandtpickerTheme
+import com.hshamkhani.persiandtpicker.picker.PersianDatePicker
+import com.hshamkhani.persiandtpicker.picker.TimePicker
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            var selectedDate by remember { mutableStateOf("") }
+            var selectedTime by remember { mutableStateOf("") }
+
             PersiandtpickerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize() ,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)
+                    ) {
+                        PersianDatePicker(
+                            onDateSelected = { jYear: Int, jMonth: Int, jDay: Int ->
+                                selectedDate = "Current date is: $jYear : $jMonth : $jDay"
+                            },
+                            textColor = MaterialTheme.colorScheme.onBackground,
+                            selectedTextColor = MaterialTheme.colorScheme.background,
+                            backGroundColor = MaterialTheme.colorScheme.background,
+                            selectedItemBackgroundColor = MaterialTheme.colorScheme.onBackground,
+                        )
+
+                        TimePicker(
+                            onTimeChanged = { hour: Int, minutes: Int, amPm: Int? ->
+                                selectedTime = "$hour:$minutes $amPm"
+                            },
+                            textColor = MaterialTheme.colorScheme.onBackground,
+                            selectedTextColor = MaterialTheme.colorScheme.background,
+                            backGroundColor = MaterialTheme.colorScheme.background,
+                            selectedItemBackgroundColor = MaterialTheme.colorScheme.onBackground,
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .clip(MaterialTheme.shapes.large)
+                                .background(MaterialTheme.colorScheme.primary)
+                                .padding(16.dp),
+                            color = Color.White,
+                            text = "$selectedDate  $selectedTime"
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PersiandtpickerTheme {
-        Greeting("Android")
     }
 }
